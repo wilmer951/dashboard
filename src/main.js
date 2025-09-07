6231f194f80dd5b7175b86def51f6b163df95f57
wilmere <wilmer-951@hotmail.com>
2025-09-07 12:17:43 -0500

    implenetacion de vue-good-table inicio

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router' // Asumiendo que tienes un archivo de configuración para el router
 import { createPinia } from 'pinia'
+import VueGoodTablePlugin from 'vue-good-table-next';
+import 'vue-good-table-next/dist/vue-good-table-next.css';
+
+
 
 // Importa tu archivo CSS principal aquí. ¡Este es el paso clave!
 import './style.css'
@@ -9,8 +13,14 @@ import './style.css'
 const app = createApp(App)
 
 const pinia = createPinia()
+
+
+
 app.use(pinia)
 
 app.use(router)
+
+app.use(VueGoodTablePlugin)
+
 
 app.mount('#app')
